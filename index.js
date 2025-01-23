1fd0976e1bad8ca5f2bc6df5d8fdd1ab42176fa2
Yousef Mahrous <y24382473@gmail.com>
2025-01-23 17:46:06 +0000

    init

--- index.js
+++ index.js
@@ -24,7 +24,38 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+// -------------------------
+const isValidDate = (date) => {
+  const parsedDate = new Date(date);
+  return !isNaN(parsedDate.getTime());
+};
+app.get('/api/:date?', function (req, res) {
+  const dateParam = req.params.date;
+  if (!dateParam) {
+    const currentDate = new Date();
+    return res.json({
+      unix: currentDate.getTime(),
+      utc: currentDate.toUTCString(),
+    });
+  }
 
+  let date;
+  if (/^\d{13}$/.test(dateParam)) {
+    date = new Date(parseInt(dateParam));
+  } else if (isValidDate(dateParam)) {
+    date = new Date(dateParam);
+  }
+
+  if (!date || isNaN(date.getTime())) {
+    return res.json({ error: "Invalid Date" });
+  }
+
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString(),
+  });
+});
+// -------------------------
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
